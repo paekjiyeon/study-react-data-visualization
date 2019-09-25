abf349a3759672a71b0e7035d20627ef29cfc71a
paekjiyeon <qor3127@naver.com>
2019-09-25 19:20:47 +0900

    file init

---
+++ knu-react-project/src/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+import Header from "./header.js";
+
+function App() {
+  return (
+    <div className="App">
+      <Header>
+
+      </Header>
+    </div>
+  );
+}
+
+export default App;
